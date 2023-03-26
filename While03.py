d435af0d4c7b7d9dec586597a8aae1cd81c66f96
ulug2002 <ulugbek_it@mail.ru>
2023-03-26 13:29:30 +0300

    done3

--- While03.py
+++ While03.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    j = 0
+    while i < len(s):
+        if s[i].isalpha():
+            j+=1
+        i+=1
+    print (s)    
+ 
