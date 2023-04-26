739dbb8b0791fe0178f8afbb32aa2927540c9b0e
ulug2002 <ulugbek_it@mail.ru>
2023-04-26 14:46:00 +0300

    d10

--- While10.py
+++ While10.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a = 0 
+    b = 0
+    while a < len(s):
+        c = int(s[a])
+        if c % 2== 1:
+            b += c
+        a += 1
+    return b
+print(main("1113"))
