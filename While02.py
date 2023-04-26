760ec33a8bb2a49cef04986d2404eb311b24b275
ulug2002 <ulugbek_it@mail.ru>
2023-04-26 12:30:45 +0300

    d2

--- While02.py
+++ While02.py
@@ -10,6 +10,7 @@ def main(s):
     n = 0
     while a < len(s):
         if s[a].isalpha():
-            n += 1
-        a += 1    
+            n = n + 1
+        a = a + 1    
     return n
+print(main("samrkand 20031243"))
